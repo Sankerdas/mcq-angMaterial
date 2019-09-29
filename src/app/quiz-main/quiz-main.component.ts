c818afd82efafb8c7eb9871eedb349d5ef91eddf
Sanker das <sankerdas.yes@gmail.com>
2019-09-29 22:52:35 +0530

    quesions listed in quiz component
    all quesions listed in quiz component in snapshotchanges method

--- src/app/quiz-main/quiz-main.component.ts
+++ src/app/quiz-main/quiz-main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz-main',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizMainComponent implements OnInit {
 
-  constructor() { }
+  constructor( private dataservice: DataService ) { }
+
+  qzList: any;
+
+  fetchQz() {
+    this.dataservice.getData().pipe(
+      map(changes => {
+        return changes.map(c => ( { key: c.payload.key, ...c.payload.val() } ));
+      }) ).subscribe(res => ( this.qzList = res ) );
+  }
 
   ngOnInit() {
+    this.fetchQz();
   }
 
 }
