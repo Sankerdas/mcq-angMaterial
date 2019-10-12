1f8ea0446da80539b6a7219024eb4527fa752d2f
Sanker das <sankerdas.yes@gmail.com>
2019-10-12 16:59:15 +0530

    fav icon changed - ready for deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyDvpHcakJgN4ERvMkpZaIJA1OO38MPpxy0',
     authDomain: 'mcqs-b0851.firebaseapp.com',
